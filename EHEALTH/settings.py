143ac6b62f1ce49b024ee3d37e87ae7904afe0de
Gilles-Déreck GNANCADJA <itgillo60@gmail.com>
2023-07-11 16:46:32 +0200

    Delete

--- EHEALTH/settings.py
+++ EHEALTH/settings.py
@@ -161,6 +161,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Front/vuehealth/node_modules/vue/dist'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
